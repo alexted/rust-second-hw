242f30dadbab65e705ca1b4a970821b4833a450a
alex tedeev <a.tedeev@codero.pro>
2022-06-27 08:48:49 +0300

    q

--- src/second.rs
+++ src/second.rs
@@ -1,21 +1,38 @@
+
 pub mod second_app {
     use std::io;
 
     pub fn convert_to_pig_latin() {
         println!("Введите фразу (eng):");
         loop {
-            let mut phraze = String::new();
+            let mut user_phraze = String::new();
 
-            io::stdin().read_line(&mut phraze).expect("Failed to read line");
+            io::stdin().read_line(&mut user_phraze).expect("Failed to read line");
 
-            let phraze: String = match phraze.trim().parse() {
+            user_phraze = match user_phraze.trim().parse() {
                 Ok(text) => text,
                 Err(_) => {
                     println!("It's not a valid text!");
                     continue;
                 }
             };
-            println!("input text is {phraze}");
+
+            let words = user_phraze.split_whitespace();
+
+            let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+            let mut encoded_phraze = String::new();
+            let encrypted_word = String::new();
+            for w in words {
+                let first_letter = &w[0..1];
+                if vowels.contains(&first_letter) {
+                    let encrypted_word = format!("{w}-hay");
+                } else {
+                    let ew = &w[1..];
+                    let encrypted_word = format!("{ew}-{first_letter}ay");
+                }
+                encoded_phraze.push_str(&format!(" {encrypted_word}"));
+            }
+            println!("{encoded_phraze}");
         }
     }
 }
