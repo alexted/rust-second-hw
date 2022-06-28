1fe0256f436b5490f88201b57057918c22e23915
zo0M <zoom@trafficstars.com>
2022-06-28 10:43:29 +0300

    finished third task

--- src/third.rs
+++ src/third.rs
@@ -1,4 +1,5 @@
 pub mod third_app {
+    use std::collections::hash_map::Entry;
     use std::collections::HashMap;
     use std::io;
 
@@ -16,7 +17,7 @@ pub mod third_app {
             user_command = match user_command.trim().parse() {
                 Ok(text) => text,
                 Err(_) => {
-                    println!("It's not a valid command!");
+                    println!("It's not a valid text command!");
                     continue;
                 }
             };
@@ -25,20 +26,42 @@ pub mod third_app {
 
             match command_words[0] {
                 ADD => {
-                    if command_words.len() < 4 {
+                    if command_words.len() < 4 || command_words[2] != "to" {
                         println!("некорректно введена команда!");
-                        continue
+                        continue;
                     };
                     let department_name = command_words[3].parse().unwrap();
                     let employer_name = command_words[1].parse().unwrap();
                     let department_unit = company.entry(department_name).or_insert(Vec::new());
                     department_unit.push(employer_name);
-                    println!("{company:#?}");
-                },
+                    // println!("{company:#?}");
+                }
                 LIST => {
-                    println!("WIP")
-                },
-                _ => println!("нет такой команды!")
+                    match command_words.len() {
+                        1 => {
+                            println!("{company:#?}");
+                            continue;
+                        }
+                        2 => {
+                            let department_name = command_words[1].parse().unwrap();
+                            match company.entry(department_name) {
+                                Entry::Occupied(department_unit) => println!("{department_unit:#?}"),
+                                _ => {
+                                    println!("Такого отдела в компании нет");
+                                    continue
+                                }
+                            }
+                        }
+                        _ => {
+                            println!("Некорректное количество аргументов переданных команде.");
+                            continue;
+                        }
+                    };
+                }
+                _ => {
+                    println!("Нет такой команды!");
+                    continue;
+                }
             }
             println!("FOR DEBUG");
         }
